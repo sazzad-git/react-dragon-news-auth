613260a85a5ef8c2f97b351af061623afd8e1b64
Sazzadur Rahman <sazzadurrahman000@gmail.com>
2024-10-09 20:45:17 +0600

    deploy firebase

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -21,6 +21,9 @@ const Navbar = () => {
         <NavLink to="/career">Career</NavLink>
       </li>
       <li>
+        <NavLink to="/addict">Addict</NavLink>
+      </li>
+      <li>
         <NavLink to="/login">Login</NavLink>
       </li>
       <li>
